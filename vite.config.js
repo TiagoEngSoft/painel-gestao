5a230eae7348d6fdb46894db41e9910403ef30ab
Tiago Santos <tiagoengsoftdev@gmail.com>
2024-05-24 10:15:29 -0300

    Correcao build

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000, // Ajuste o limite de aviso de tamanho de pedaço conforme necessário
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            // Agrupa todas as dependências de terceiros em um único pedaço (chunk)
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
 })
